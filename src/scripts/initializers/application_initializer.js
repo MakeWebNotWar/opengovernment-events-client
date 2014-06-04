6aeac34c0589a880ae317c1c7f245ba4f8931af0
Raymond Kao <ray@peopleandcode.com>
2014-06-03 22:20:58 -0400

    added custom authorizer in app initializer

--- src/scripts/initializers/application_initializer.js
+++ src/scripts/initializers/application_initializer.js
@@ -2,14 +2,10 @@ Ember.Application.initializer({
   name: 'authentication',
   initialize: function(container, application) {
     container.register('authenticator:custom', Opengov.CustomAuthenticator);
-    Ember.SimpleAuth.setup(container, application);
-    
-    Opengov.Session = Ember.Object.extend({
-      init: function(){
-        this._super();
-        var session = container.lookup('controller:login').get('session');
-        this.set('authToken', session.get('authentication_token'));
-      }
-    }).create();
+    container.register('authorizer:custom', Opengov.CustomAuthorizer);
+
+    Ember.SimpleAuth.setup(container, application, {
+      authorizerFactory: 'authorizer:custom'
+    });
   }
 });
