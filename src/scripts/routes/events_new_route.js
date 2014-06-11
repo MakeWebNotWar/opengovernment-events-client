6df081ec18e171f3dfd8059fef236a8540ea97de
Raymond Kao <ray@peopleandcode.com>
2014-06-11 02:58:29 -0400

    updated new event creation transition

--- src/scripts/routes/events_new_route.js
+++ src/scripts/routes/events_new_route.js
@@ -1,2 +1,11 @@
 Opengov.EventsNewRoute = Ember.Route.extend({
+  beforeModel: function(transition){
+    var self, session;
+
+    session = this.get('session');
+    session.set('attemptedTransition', transition);
+    if(!session.isAuthenticated){
+      this.transitionTo('login');
+    }
+  }
 });
